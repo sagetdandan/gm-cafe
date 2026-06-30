05148855934a5fd5ad7c60df6f7dcb2ec31f05c3
sagetdandan <sagetdandan@gmail.com>
2026-06-30 13:28:05 +0700

    Cleanup code and add flutter doctor to workflow

--- lib/db_setup_stub.dart
+++ lib/db_setup_stub.dart
@@ -3,18 +3,30 @@ import 'models.dart';
 Future<void> setupDatabaseFactory() async {}
 Future<String> getDbPath(String name) async => name;
 Future<dynamic> openMobileDatabase(String path, Function onCreate) async => null;
+
 Future<List<Category>> getMobileCategories(dynamic db) async => [];
+Future<void> insertMobileCategory(dynamic db, Category c) async {}
+
 Future<List<CafeTable>> getMobileTables(dynamic db) async => [];
 Future<void> insertMobileTable(dynamic db, CafeTable t) async {}
 Future<void> updateMobileTable(dynamic db, CafeTable t) async {}
 Future<void> deleteMobileTable(dynamic db, int id) async {}
+
+Future<List<AppUser>> getMobileUsers(dynamic db) async => [];
+Future<void> insertMobileUser(dynamic db, AppUser u) async {}
+Future<void> updateMobileUser(dynamic db, AppUser u) async {}
+Future<void> deleteMobileUser(dynamic db, int id) async {}
+Future<AppUser?> loginMobile(dynamic db, String u, String p) async => null;
+
 Future<List<Product>> getMobileProductsByCategory(dynamic db, int id) async => [];
 Future<List<Product>> getMobileAllProducts(dynamic db) async => [];
 Future<void> insertMobileProduct(dynamic db, Product p) async {}
 Future<void> updateMobileProduct(dynamic db, Product p) async {}
+
 Future<void> insertMobileTransaction(dynamic db, TransactionHeader h, List<CartItem> i) async {}
 Future<List<TransactionHeader>> getMobileTransactions(dynamic db, DateTime? s, DateTime? e) async => [];
 Future<List<Map<String, dynamic>>> getMobileTransactionDetails(dynamic db, int id) async => [];
+
 Future<int> insertMobileExpense(dynamic db, Expense e) async => 0;
 Future<List<Expense>> getMobileExpenses(dynamic db, DateTime? s, DateTime? e) async => [];
 Future<void> deleteMobileExpense(dynamic db, int id) async {}
